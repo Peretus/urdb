c8602123639a8f2723e3c6a142fb7107a37fe06e
Casey McNeil <caseymcneil@gmail.com>
2013-12-12 11:19:06 -0600

    add failing method for audience_rating Movie method

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -4,7 +4,7 @@ describe Movie do
   let(:movie) {Movie.new}
 
   describe "#snippet" do
-    context "When description is less than or equal to 50 chars" do
+    context "when description is less than or equal to 50 chars" do
       it "returns the full description" do
         allow(movie).to receive(:description).and_return("Hey there I'm less than 50")
 
@@ -26,6 +26,17 @@ describe Movie do
 
         expect(movie.snippet).to eq ""
       end
+    end
+  end
+
+  describe '#audience_rating' do
+    let(:rotten_finder_return) { OpenStruct.new(ratings: OpenStruct.new(audience_score: 84)) }
+    context "when Movie is found on Rotten Tomatoes" do
+      it "returns the audience score for the Movie" do
+        allow(movie).to receive(:rotten_finder).and_return(rotten_finder_return)
+
+        expect(movie.audience_rating).to eq(84)
+      end   
     end
   end
 end
